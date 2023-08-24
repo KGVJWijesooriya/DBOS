0c81ee1eaa4e2da8b8adbce1ff84c76882e5fdb2
KGVJWijesooriya <vimukthi200020@gmail.com>
2023-08-24 10:52:10 +0530

    invoice

--- Admin/admin/add_stocks.php
+++ Admin/admin/add_stocks.php
@@ -10,10 +10,10 @@
             <a href="add_vendor.php" class="btn">Add Vendors</a>
         </div>
         <div>
-            <form>
+            <form method="post">
                 <div class="form-row">
                     <div class="col-4">
-                        <!-- <input type="hidden" name="search" id="search" class="form-control" placeholder="Vendor Name" autocomplete="off" required><br> -->
+                        <input type="hidden" name="Vender_ID" id="Vender_ID" value="">
                         <select name="Vender_Name" data-live-search="true" id="Vender_Name" class="form-control" title="Select Vender Name"> </select> <br>
                     </div>
                     <!-- <div>
@@ -28,9 +28,8 @@
                         <br><input type="date" class="form-control" required>
                     </div>
                 </div>
-            </form>
         </div>
-
+        
         <div class=" mt-4">
             <table class="table table-striped">
                 <thead>
@@ -42,44 +41,73 @@
                         <td>Amount</td>
                     </tr>
                 </thead>
-                <form>
-                    <tbody id="tbl">
-                        <tr>
-                            <td class="count"></td>
-                            <td class="col-4">
-                                <select name="Product_box" id="Product_box" class="form-control " data-live-search="true" title="Select Product Name"></select>
-                            </td>
-                            <td>
-                                <input type="number" class="form-control" name="qty" onchange="Calc(this);">
-                            </td>
-                            <td>
-                                <input type="number" class="form-control" name="cost" onchange="Calc(this);">
-                            </td>
-                            <td>
-                                <input type="number" class="form-control" name="amount">
-                            </td>
-                        </tr>
-                    </tbody>
-                </form>
+                <tbody id="tbl">
+                    <tr>
+                        <td class="count"></td>
+                        <td class="col-4">
+                            <select name="Product_box" id="Product_box" class="form-control " data-live-search="true" title="Select Product Name"></select>
+                        </td>
+                        <td>
+                            <input type="number" class="form-control" name="qty" onchange="Calc(this);">
+                        </td>
+                        <td>
+                            <input type="number" class="form-control" name="cost" onchange="Calc(this);">
+                        </td>
+                        <td>
+                            <input type="number" class="form-control" name="amount">
+                        </td>
+                    </tr>
+                </tbody>
+
             </table>
             <div class="float-right">
                 <h4 class="float-right">Total</h4><br>
-                <input type="number" class="tota" id="total" readonly>
-                <input type="submit" name="save" id="save" class="button add_another btn btn-success" value="Save" />
+                <input type="number" class="tota" id="total" name="total" value="" readonly>
+                <input type="submit" name="submit" id="save" class="button add_another btn btn-success" value="Save" />
             </div>
-            <input type="submit" class="button add_another btn btn-success" value="Add another line" />
+            <input type="submit" id="addLine" class="button add_another btn btn-success" value="Add another line" />
+            </form>
         </div>
     </div>
 </div>
 
-
 <script>
-    // $('document').ready(function() {
-    //     $('.add_another').click(function() {
-    //         $("#tbl").append('<tr><td class="count"></td><td><input type="text" name="search" id="search" class="form-control" placeholder="Enter Product Name or Barcode" autocomplete="off" required></td><td><input type="number" class="form-control" name="qty" onchange="Calc(this);"></td><td><input type="number" class="form-control" name="cost" onchange="Calc(this);"></td><td><input type="number" class="form-control" name="amount"></td></tr>');
-    //     });
-    // })
+    $(document).ready(function() {
+        load_data("ProductData"); // Change the type to "ProductData"
+
+        function load_data(type = "") {
+            $.ajax({
+                url: "fetch.php", // Change the URL to the correct PHP file
+                method: "POST",
+                data: {
+                    type: type,
+                },
+                dataType: "json",
+                success: function(data) {
+                    var html = "";
+                    for (var count = 0; count < data.length; count++) {
+                        html += '<option value="' + data[count].id + '">' + data[count].name + ' (ID: ' + data[count].id + ')</option>';
+                    }
+                    $("#tbl").append('<tr><td class="count"></td><td class="col-4"><select name="Product_box" class="form-control" data-live-search="true" title="Select Product Name">' + html + '</select></td><td><input type="number" class="form-control" name="qty" onchange="Calc(this);"></td><td><input type="number" class="form-control" name="cost" onchange="Calc(this);"></td><td><input type="number" class="form-control" name="amount"></td></tr>');
+
+                    // Refresh the Bootstrap-select in the newly added row
+                    $("select[name='Product_box']").last().selectpicker('refresh');
+
+                    // Recalculate the count in each row
+                    updateRowCounts();
+                },
+            });
+        }
+
+        // Function to update the count in each row
+        function updateRowCounts() {
+            $("td.count").each(function(index) {
+                $(this).text(index + 1);
+            });
+        }
+    });
 </script>
+
 <!-- Calculation -->
 <script>
     function Calc(v) {
@@ -126,44 +154,88 @@
                     var html = "";
                     for (var count = 0; count < data.length; count++) {
                         html += '<option value="' + data[count].id + '">' + data[count].name + ' (ID: ' + data[count].id + ')</option>';
-
-                        $("#Vender_Name").html(html);
-                        $("#Vender_Name").selectpicker("refresh");
                     }
+                    $("#Vender_Name").html(html);
+                    $("#Vender_Name").selectpicker("refresh");
+                },
+            });
+        }
+
+        // Add an onchange event to update the hidden input (Vender_ID)
+        $("#Vender_Name").on("changed.bs.select", function(e) {
+            var selectedOption = $(this).find("option:selected");
+            var vendorID = selectedOption.val();
+            $("#Vender_ID").val(vendorID);
+        });
+    });
+
+
+
+    // Product Data
+
+    $(document).ready(function() {
+        $("#Product_box").selectpicker();
+
+        load_data("ProductData"); // Change the type to "ProductData"
+
+        function load_data(type = "") {
+            $.ajax({
+                url: "fetch.php", // Change the URL to the correct PHP file
+                method: "POST",
+                data: {
+                    type: type,
+                },
+                dataType: "json",
+                success: function(data) {
+                    var html = "";
+                    for (var count = 0; count < data.length; count++) {
+                        html += '<option value="' + data[count].id + '">' + data[count].name + ' (ID: ' + data[count].id + ')</option>';
+                    }
+                    $("#Product_box").html(html);
+                    $("#Product_box").selectpicker("refresh");
                 },
             });
         }
     });
-
-
-// Product Data
-
-$(document).ready(function() {
-    $("#Product_box").selectpicker();
-
-    load_data("ProductData"); // Change the type to "ProductData"
-
-    function load_data(type = "") {
-        $.ajax({
-            url: "fetch.php", // Change the URL to the correct PHP file
-            method: "POST",
-            data: {
-                type: type,
-            },
-            dataType: "json",
-            success: function(data) {
-                var html = "";
-                for (var count = 0; count < data.length; count++) {
-                    html += '<option value="' + data[count].id + '">' + data[count].name + ' (ID: ' + data[count].id + ')</option>';
-                }
-                $("#Product_box").html(html);
-                $("#Product_box").selectpicker("refresh");
-            },
-        });
-    }
-});
-
 </script>
+
+<?php
+
+if (isset($_POST['submit'])) {
+    $vendor_id = $_POST['Vender_ID'];
+    $new_total = $_POST['total'];
+    
+    // Fetch the current amount from the database
+    $sql_select = "SELECT Amount FROM vendor WHERE id = '$vendor_id'";
+    $result_select = mysqli_query($conn, $sql_select);
+
+    if ($result_select) {
+        $row = mysqli_fetch_assoc($result_select);
+        $current_amount = $row['Amount'];
+        
+        // Calculate the new total amount
+        $total_amount = $current_amount + $new_total;
+        
+        // Perform SQL update query
+        $sql_update = "UPDATE vendor SET Amount = '$total_amount' WHERE id = '$vendor_id'";
+        $result_update = mysqli_query($conn, $sql_update);
+
+        if ($result_update) {
+            // Success message
+            echo "Vendor data updated successfully!";
+        } else {
+            // Error message
+            echo "Error updating vendor data: " . mysqli_error($conn);
+        }
+    } else {
+        // Error message for fetching current amount
+        echo "Error fetching current amount: " . mysqli_error($conn);
+    }
+}
+
+
+
+?>
 
 
 
--- Admin/admin/footer.php
+++ Admin/admin/footer.php
@@ -11,3 +11,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+
+<!-- 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Include Bootstrap (once) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Include Bootstrap Select (once) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0/js/bootstrap-select.min.js"></script> -->
